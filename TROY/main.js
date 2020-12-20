c954208bd4a690f84df13f1ccba84f4d38fc584b
mohamed4ahmed <75965434+mohamed4ahmed@users.noreply.github.com>
2020-12-20 02:17:28 +0200

    up

---
+++ TROY/main.js
@@ -0,0 +1,28 @@
+var daysEl =document.getElementById("days");
+var hoursEl =document.getElementById("hours");
+var minsEl =document.getElementById("minutes");
+var secondsEl =document.getElementById("seconds");
+
+var newYears = new Date("2021 1 1");
+
+function countdown() {
+
+    var currentDate = new Date ();
+  //  var  newYearDate = (newYears);
+    
+
+    var  totalSeconds = Math.floor( (newYears - currentDate)/1000 );
+    var  seconds = Math.floor(totalSeconds)%60;
+    var  mins   =  Math.floor( totalSeconds/60 ) %60;
+    var  hours   =  Math.floor( totalSeconds/ 60/60 )%24;
+    var  days   = Math.floor( totalSeconds/60/60/24 );
+
+    daysEl.innerHTML= days;
+    hoursEl.innerHTML= hours;
+    minsEl.innerHTML= mins;
+    secondsEl.innerHTML= seconds;
+}
+// initial call
+countdown();
+
+setInterval(countdown);
